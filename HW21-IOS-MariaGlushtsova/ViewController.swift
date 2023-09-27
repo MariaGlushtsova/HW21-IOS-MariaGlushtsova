40dcd53d15c2902965f708759c780accc166ff32
MariaGlushtsova <glushtsova@tut.by>
2023-09-27 12:24:47 +0300

    Initial Commit

---
+++ HW21-IOS-MariaGlushtsova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW21-IOS-MariaGlushtsova
+//
+//  Created by Admin on 27.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
